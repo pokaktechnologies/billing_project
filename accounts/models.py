9ecd00128a4fd747a746c660976a7f067666db22
Anas Mohammed <anasmuhammed444@gmail.com>
2025-04-17 21:01:04 +0300

    product model cgst & sgst

--- accounts/models.py
+++ accounts/models.py
@@ -179,6 +179,8 @@ class Product(models.Model):
     unit = models.ForeignKey('Unit', on_delete=models.CASCADE, related_name='products')  # Connected as ForeignKey
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='products')
+    sgst = models.DecimalField(max_digits=12, decimal_places=2, default=0, help_text="SGST percentage (e.g. 9.00)")  
+    cgst = models.DecimalField(max_digits=12, decimal_places=2, default=0, help_text="CGST percentage (e.g. 9.00)")  
 
     def __str__(self):
         return self.name

05ff2aa0fe2e259f8a518234112d2ec1cceca4c2
MazwinBachi <mazwinb8@gmail.com>
2025-06-06 19:08:23 +0530

    June 6: Changed UserModulesView, now for superuser it will return all choices with an extra value 'users'.

--- accounts/views/user.py
+++ accounts/views/user.py
@@ -51,5 +51,11 @@ class UserModulesView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        modules = ModulePermission.objects.filter(user=request.user).values_list('module_name', flat=True)
-        return Response({"modules": list(modules)})
+        if request.user.is_superuser:
+            # Return all possible modules if user is admin
+            all_modules = [choice[0] for choice in ModulePermission.MODULE_CHOICES]
+            all_modules.append("users")
+            return Response({"modules": all_modules})
+        else:
+            modules = ModulePermission.objects.filter(user=request.user).values_list('module_name', flat=True)
+            return Response({"modules": list(modules)})

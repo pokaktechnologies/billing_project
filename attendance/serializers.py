8f92713214d7dcc6b8c05bd22243e3a6d75a35f0
Ihsan ali <ihsanmuhammedalik@gmail.com>
2025-10-04 14:32:59 +0530

    Update serializers.py
    add employee details

--- attendance/serializers.py
+++ attendance/serializers.py
@@ -14,7 +14,13 @@ class AttendanceSessionSerializer(serializers.ModelSerializer):
 
 class DailyAttendanceSerializer(serializers.ModelSerializer):
     sessions = AttendanceSessionSerializer(many=True, read_only=True)
+    
+    first_name = serializers.CharField(source='staff.user.first_name', read_only=True)
+    last_name = serializers.CharField(source='staff.user.last_name', read_only=True)
+    employee_id = serializers.CharField(source='staff.job_detail.employee_id', read_only=True)
+    department = serializers.CharField(source='staff.job_detail.department.name', read_only=True)
+
 
     class Meta:
         model = DailyAttendance
-        fields = ['id', 'staff', 'date', 'total_working_hours', 'status', 'sessions']
+        fields = ['id', 'staff','first_name', 'last_name','employee_id','department', 'date', 'total_working_hours', 'status', 'sessions']

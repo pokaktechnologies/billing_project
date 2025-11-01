96b922467ec1e61228af8e51fcf9f697fda64891
Sreehari Jayesh <sreeharivj01@gmail.com>
2025-11-01 13:00:06 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -131,7 +131,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("172.23.53.193", 6379)],
+            "hosts": [("127.0.0.1", 6379)],
         },
     },
 }

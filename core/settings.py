7b594079fe649a24daf4a5232514a6e5fd7f97d2
anfiquehussain <adomax2003@gmail.com>
2025-12-15 14:29:59 +0530

    FILE_UPLOAD_MAX_MEMORY_SIZE

--- core/settings.py
+++ core/settings.py
@@ -203,3 +203,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 50 * 1024 * 1024   # 50 MB
+FILE_UPLOAD_MAX_MEMORY_SIZE = 50 * 1024 * 1024   # 50 MB
+
+FILE_UPLOAD_HANDLERS = [
+    "django.core.files.uploadhandler.TemporaryFileUploadHandler",
+]

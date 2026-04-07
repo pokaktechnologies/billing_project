30f1b9171242374ad15518ce38a55c1ca08f3369
anfiquehussain <adomax2003@gmail.com>
2026-04-07 12:03:38 +0530

    activity log new manual post, invoice 5 decimal support unit price

--- activity_logs/urls.py
+++ activity_logs/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('activity-logs/', views.ActivityLogListView.as_view(), name='activity-log-list'),
+    path('create/', views.ActivityLogCreateAPIView.as_view(), name='activity-log-create'),
 ]
